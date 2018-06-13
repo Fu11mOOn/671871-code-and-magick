bc92469ab3d6f196c7c61a479f042226dc6d8090
Иван <vanoza_007@mail.ru>
2018-06-13 20:46:33 +0600

    Закончил задание 'Одеть Надежду'

--- js/setup.js
+++ js/setup.js
@@ -35,7 +35,16 @@ var EYES_COLORS = [
   'yellow',
   'green'
 ];
+var FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
 var NUMBER_OF_WIZARDS = 4;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var getRandomIntegerFromInterval = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -72,12 +81,77 @@ var pasteWizards = function (array) {
 
   elementsList.appendChild(fragment);
 };
+var onSetupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    onSetupClose();
+  }
+};
+var onSetupOpen = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onSetupEscPress);
+  setupCloseButton.addEventListener('keydown', onSetupEscPress);
+};
+var onSetupClose = function () {
+  setup.classList.add('hidden');
+};
+var getNextElement = function (array, element) {
+  var currentElement = array.indexOf(element) === -1 ? array[0] : array[array.indexOf(element) + 1];
 
-var setupWizard = document.querySelector('.setup');
+  if (currentElement === array[array.length - 1]) {
+    currentElement = array[0];
+  }
+
+  return currentElement;
+};
+var alternatelyChangeFillColorOfWizardParameterWhenPressed = function (wizardParameter, arrayWithColors, input) {
+  wizardParameter.addEventListener('click', function (evt) {
+    if (evt.target === wizardParameter) {
+      var pastColor = wizardParameter.style.fill;
+      var newColor = getNextElement(arrayWithColors, pastColor);
+
+      wizardParameter.style.fill = newColor;
+      input.value = newColor;
+    }
+  });
+};
+var alternatelyChangeBackgroundColorOfWizardParameterWhenPressed = function (wizardParameter, arrayWithColors, input) {
+  wizardParameter.addEventListener('click', function (evt) {
+    if (evt.currentTarget === wizardParameter) {
+      var pastColor = input.value;
+      var newColor = getNextElement(arrayWithColors, pastColor);
+
+      wizardParameter.style.backgroundColor = newColor;
+      input.value = newColor;
+    }
+  });
+};
+
+var mockOfWizards = generateWizards(NUMBER_OF_WIZARDS);
 var setupSimilarWizard = document.querySelector('.setup-similar');
-var wizards = generateWizards(NUMBER_OF_WIZARDS);
+var setup = document.querySelector('.setup');
+var setupOpenButton = document.querySelector('.setup-open');
+var setupCloseButton = document.querySelector('.setup-close');
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var inputOfwizardCoat = document.querySelector('input[name="coat-color"]');
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+var inputOfwizardEyes = document.querySelector('input[name="eyes-color"]');
+var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
+var inputOfFireballWrap = document.querySelector('input[name="fireball-color"]');
 
-setupWizard.classList.remove('hidden');
+pasteWizards(mockOfWizards);
 setupSimilarWizard.classList.remove('hidden');
-
-pasteWizards(wizards);
+setupOpenButton.addEventListener('click', onSetupOpen);
+setupOpenButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onSetupOpen();
+  }
+});
+setupCloseButton.addEventListener('click', onSetupClose);
+setupCloseButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onSetupClose();
+  }
+});
+alternatelyChangeFillColorOfWizardParameterWhenPressed(wizardCoat, COAT_COLORS, inputOfwizardCoat);
+alternatelyChangeFillColorOfWizardParameterWhenPressed(wizardEyes, EYES_COLORS, inputOfwizardEyes);
+alternatelyChangeBackgroundColorOfWizardParameterWhenPressed(setupFireballWrap, FIREBALL_COLORS, inputOfFireballWrap);
